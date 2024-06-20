820fcb3c2ab185126d2e786fcae7ff07c4b7d5b5
Gabriel Moreira <gabrielmscampos@gmail.com>
2024-06-20 17:49:34 +0200

    chore: update push_containers script to allow pushing images as prod or dev

--- push_containers.sh
+++ push_containers.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+ENV=$1
 REGISTRY_REPO=registry.cern.ch/cms-dqmdc
 
+if [ -z "$1" ]; then
+    echo "Error: ENV variable not set. Please specify 'prod' or 'dev'."
+    exit 1
+fi
+
+if [ "$ENV" = "prod" ]; then
+    echo "Environment is production."
+elif [ "$ENV" = "dev" ]; then
+    echo "Environment is development."
+else
+    echo "Environment is neither production nor development."
+    exit 1
+fi
+
 # Build
-docker build -f ./backend/Dockerfile -t $REGISTRY_REPO/hdqm-backend .
-docker build -f ./frontend/Dockerfile -t $REGISTRY_REPO/hdqm-frontend .
+docker build -f ./backend/Dockerfile -t $REGISTRY_REPO/hdqm-backend-$ENV .
+docker build -f ./frontend/Dockerfile -t $REGISTRY_REPO/hdqm-frontend-$ENV .
 
 # login to registry and push containers
 docker login https://registry.cern.ch
-docker push $REGISTRY_REPO/hdqm-backend
-docker push $REGISTRY_REPO/hdqm-frontend
+docker push $REGISTRY_REPO/hdqm-backend-$ENV
+docker push $REGISTRY_REPO/hdqm-frontend-$ENV
 
